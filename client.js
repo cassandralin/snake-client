f19efca1af518e7054f4557600cce4b43c6cd46d
cassandralin <cassandralin92@gmail.com>
2020-01-15 20:34:29 +0000

    finished constants module with Roha and Christo

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants')
+ 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -19,3 +20,4 @@ const connect = function() {
 }
 
 connect();
+module.exports = { connect };
